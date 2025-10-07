2404d541b541a24acb8342081a7db8aff202db44
Malharii <mrmalhari96@gmail.com>
2025-10-07 15:55:39 +0530

    Added code for created a new project on the backend

--- backend/src/routes/v1/index.js
+++ backend/src/routes/v1/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { pingCheck } from "../../controllers/pingControllers.js";
+import projectsRouter from "./projects.js";
 
 const router = express.Router();
 
 router.use("/ping", pingCheck);
+router.use("/projects", projectsRouter);
 
 export default router;
