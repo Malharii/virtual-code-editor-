37ee6e966ad5f661bea0d883ea7e74a71ac47141
Malharii <mrmalhari96@gmail.com>
2025-10-10 12:59:56 +0530

    editor componet is added and theme part is added

--- frontend/src/components/molecules/EdditerComponet/EdditorComponet.jsx
+++ frontend/src/components/molecules/EdditerComponet/EdditorComponet.jsx
@@ -1,3 +1,43 @@
+import { Editor } from "@monaco-editor/react";
+import { useEffect, useState } from "react";
 export const EditorComponet = () => {
-  return <>editor</>;
+  const [editorStore, setEditorStore] = useState({
+    theme: null,
+  });
+
+  async function downloadTheme() {
+    try {
+      const response = await fetch("/Dracula.json");
+      if (!response.ok) throw new Error("Failed to load theme");
+      const data = await response.json();
+      setEditorStore({ theme: data });
+    } catch (err) {
+      console.error("Error loading theme:", err);
+    }
+  }
+
+  function handleEditorTheme(editor, monaco) {
+    if (editorStore.theme) {
+      monaco.editor.defineTheme("dracula", editorStore.theme);
+      monaco.editor.setTheme("dracula");
+    }
+  }
+
+  useEffect(() => {
+    downloadTheme();
+  }, []);
+  return (
+    <>
+      {editorStore.theme && (
+        <Editor
+          height={"80vh"}
+          width={"100%"}
+          defaultLanguage="javascript"
+          defaultValue="//Welcome to PlayGround"
+          options={{ fontSize: 18, fontFamily: "monospace" }}
+          onMount={handleEditorTheme}
+        />
+      )}
+    </>
+  );
 };
